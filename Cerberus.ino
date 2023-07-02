38a4e57c02590517bc77d609ad8422205d63f3dd
Engum <97731980+Engum@users.noreply.github.com>
2023-07-02 17:57:30 +0200

    Calibration day 1

--- Cerberus.ino
+++ Cerberus.ino
@@ -16,21 +16,29 @@ const int trigPin = 5;
 const int echoPin = 18;
 const int sirenPin = 25;
 
-
 //define sound speed in cm/uS
 #define SOUND_SPEED 0.034
-#define SIREN_TIMER 100 //4000 //Time for siren to be active in ms
-#define DM_TIMER 1000
+#define SIREN_TIMER 4000 //4000 //Time for siren to be active in ms
+#define DM_TIMER 20       // how often to meassure distance  and run distanceCheck
+#define DM_ArraySize 20  //HIGHER IS MORE SENSITIVE default: 20 // ammount of meassurements that are checked for movement. Higher number gives more time-range to detect movement        
+#define MA_ArraySize 20  //
+//alarm sensitivity-requirement settings: LOWER IS MORE SENSITIVE. (more alarm)
+#define DISTANCE_GAP_SENSITIVITY 2 // required distance difference for alarm-trip default: 2   aprox: number*10cm  example: DISTANCE_GAP_SENSITIVITY at 2 means distance has to change 40cm from measure to measure to upp the treatlevel by one. 
+#define MEASURE_COUNT_SENSITIVITY 5 // ammout of required threatLevel-points, default: 5 choose between 0 and DM_ArraySize - 1      4 or less-unstable
+#define MISSREAD_LIMIT 70 // exclude unreasonable jumps in measurements from the detection logic
 
 long duration;
 float distanceCm;
-float prevDistanceCm;
+int distanceCmInt;
 bool sirenStatus = false;
 unsigned long sirenStartMillis = millis();
 unsigned long prevDistanceMeassure = millis();
 enum systemState {active = 0, inactive = 1};
-systemState SystemState = inactive;
-
+systemState SystemState = active;
+int distanceVals[DM_ArraySize];
+int threatLevel = 0;
+int distanceAggreagte = 0;
+int movingAverage[MA_ArraySize];
 
 WiFiClientSecure client;
 UniversalTelegramBot bot(BOTtoken, client);
@@ -40,15 +48,9 @@ int botRequestDelay = 14000;
 unsigned long lastTimeBotRan;
 String chat_id;
 
-const int foto = 32;
-int buzzState = 0;
-int magnetState = analogRead(foto); 
-
 void setup() {
   Serial.begin(115200);
-
-  pinMode(2, OUTPUT);
-  pinMode(foto, INPUT);
+  pinMode(2, OUTPUT); // Sets the ledPin for system testing as an Output
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   pinMode(sirenPin, OUTPUT); // Sets the trigPin as an Output
@@ -74,17 +76,17 @@ void loop() {
   switch (SystemState) {
     case active:
       getDistance();
-      distanceCheck();
       break;
     case inactive:
-      // statements
+      sirenOFF();
       break;
   }
 }
+
+
 //-----------------------Functions--------------------//
-
-
 void getDistance() {
+  if   (millis() >= prevDistanceMeassure + DM_TIMER){
     // Clears the trigPin
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
@@ -98,46 +100,114 @@ void getDistance() {
     
     // Calculate the distance
     distanceCm = duration * SOUND_SPEED/2;
+    distanceCmInt = int(distanceCm/10);
+
       
     // Prints the distance in the Serial Monitor
-    
-  if   (millis() <= prevDistanceMeassure + DM_TIMER){
-    Serial.print("Distance (dm): ");
-    Serial.println(int(distanceCm/10));
+//    Serial.print("Distance (dm): ");
+//    Serial.println(int(distanceCm/10));
     prevDistanceMeassure = millis();
+    distanceCheck(distanceCmInt);
   }
-}
+}void distanceCheckBACKUP(int distanceCmInt) {
+  for (int i = 0; i < DM_ArraySize-1; i++) {
+      distanceVals[DM_ArraySize-1-i] = distanceVals[DM_ArraySize-2-i]; //exclude distanceVals[0]
+  }distanceVals[0] = distanceCmInt;                                    //add distanceVals[0]
+  
+  for (int i = 0; i < DM_ArraySize; i++) {
+      Serial.print(distanceVals[i]);
+  }Serial.println("");  
 
+  for (int i = 0; i < DM_ArraySize-1; i++) {
+    if (abs(distanceVals[i]-distanceVals[i+1]) >= DISTANCE_GAP_SENSITIVITY){
+      threatLevel++;
+      if ((abs(distanceVals[0]-distanceVals[i+1]) <= DISTANCE_GAP_SENSITIVITY) && (abs(distanceVals[i]-distanceVals[i+1]) <= MISSREAD_LIMIT)){
+      distanceAggreagte += abs(distanceVals[i]-distanceVals[i+1]);
+      //Serial.print(String(distanceAggreagte) + " ");
+      }
+    }
+  }    
 
-void distanceCheck() {
-  if (int(distanceCm)/10 != prevDistanceCm){
+  if (threatLevel >= MEASURE_COUNT_SENSITIVITY){
     runSiren(true);  //start siren                   --set sirentime in define SIREN_TIMER
+    Serial.println(""); 
   }else{
     runSiren(false); //check if siren should turn off
   }  
-  prevDistanceCm = int(distanceCm)/10;
+  threatLevel = 0;
+  distanceAggreagte = 0;
+}
+void distanceCheck(int distanceCmInt) {
+  //-----------------------ShiftReg------------------//
+  for (int i = 0; i < DM_ArraySize-1; i++) {
+      distanceVals[DM_ArraySize-1-i] = distanceVals[DM_ArraySize-2-i]; //exclude distanceVals[0]
+  }distanceVals[0] = distanceCmInt;                                    //add distanceVals[0]
+  //-----------------------Printer------------------//
+/*  
+  for (int i = 0; i < DM_ArraySize; i++) {
+      Serial.print(distanceVals[i]);
+  }Serial.println("");  //*/
+
+  //-----------------------MovingAverage------------------//  
+  for (int i = 0; i < MA_ArraySize-1; i++) {
+      movingAverage[MA_ArraySize-1-i] = movingAverage[MA_ArraySize-2-i]; //exclude movingAverage[0]
+  }
+  for (int i = 0; i < DM_ArraySize-1; i++) {
+    movingAverage[0] += distanceVals[i];
+  }
+  for (int i = 0; i < DM_ArraySize; i++) {
+    Serial.print(movingAverage[i]);
+  }Serial.println("");  //*/
+  
+for (int i = 0; i < DM_ArraySize-1; i++) {  
+  if (abs(movingAverage[0] - movingAverage[1]) >= DISTANCE_GAP_SENSITIVITY){
+    threatLevel++;
+    if ((abs(distanceVals[0]-distanceVals[i+1]) <= DISTANCE_GAP_SENSITIVITY) && (abs(distanceVals[i]-distanceVals[i+1]) <= MISSREAD_LIMIT)){
+    distanceAggreagte += abs(distanceVals[i]-distanceVals[i+1]);
+    }
+  }
 }
 
+  
+  //-----------------------DetectionLogic------------------//
+  for (int i = 0; i < DM_ArraySize-1; i++) {
+    if (abs(distanceVals[i]-distanceVals[i+1]) >= DISTANCE_GAP_SENSITIVITY){
+      threatLevel++;
+      if ((abs(distanceVals[0]-distanceVals[i+1]) <= DISTANCE_GAP_SENSITIVITY) && (abs(distanceVals[i]-distanceVals[i+1]) <= MISSREAD_LIMIT)){
+      distanceAggreagte += abs(distanceVals[i]-distanceVals[i+1]);
+      //Serial.print(String(distanceAggreagte) + " ");
+      }
+    }
+  }    
+
+  if (threatLevel >= MEASURE_COUNT_SENSITIVITY){
+    runSiren(true);  //start siren                   --set sirentime in define SIREN_TIMER
+    Serial.println(""); 
+  }else{
+    runSiren(false); //check if siren should turn off
+  }  
+  threatLevel = 0;
+  distanceAggreagte = 0;
+}
 void runSiren(bool reset){
   sirenStatus = reset;
   if (sirenStatus == true){
     sirenON();
     sirenStatus = false;
-    bot.sendMessage(CHAT_ID, "ALARM utløst", "");
+   // bot.sendMessage(CHAT_ID, "ALARM utløst", "");
     sirenStartMillis = millis();
   }else if (millis() > sirenStartMillis + SIREN_TIMER){
     sirenOFF();
     sirenStatus = false;
   }
 }
-
 void sirenToggle(){
   digitalWrite(sirenPin, !digitalRead(sirenPin));
   delay(1000);
 }
 void sirenON(){
   digitalWrite(sirenPin, LOW);
-  Serial.println("siren on   ++++++");
+  //Serial.println("siren on   ++++++");
 }
 void sirenOFF(){
   digitalWrite(sirenPin, HIGH);
@@ -161,7 +231,15 @@ void sirenBURST(){
   digitalWrite(sirenPin, HIGH);
   delay(100);
 }
-
+String getSystemState(){
+  if (SystemState == 1){
+    return "Asleep";
+  }else if (SystemState == 0){
+    return "in Senty-Mode";
+  }else{
+    return "in errorstate: Out of state";
+  }
+}
 void checkMessages(){
     if (millis() > lastTimeBotRan + botRequestDelay)  {
     int numNewMessages = bot.getUpdates(bot.last_message_received + 1);
@@ -174,7 +252,6 @@ void checkMessages(){
     lastTimeBotRan = millis();
   }
 }
-
 void handleNewMessages(int numNewMessages) {
   Serial.println("handleNewMessages");
   Serial.println(String(numNewMessages));
@@ -195,27 +272,25 @@ void handleNewMessages(int numNewMessages) {
 
 
     if (text == "/start") {
-      String welcome = "Welcome, " + from_name + ".\n";
-      welcome += "System is currently \n\n";
+      String welcome = "Welcome, master " + from_name + ".\n";
+      welcome += "Cerberus is currently " + getSystemState() + " \n\n";
       welcome += "Use the following commands to control your outputs.\n\n";
       welcome += "/System_test to flash onboard LED \n";
       welcome += "/siren_burst to play a butiful siren hymn \n";
       welcome += "/status to request current GPIO status \n";
+      welcome += "/awaken to activate sentry-mode \n";
+      welcome += "/sleep to deactivate \n";
       bot.sendMessage(chat_id, welcome, "");
     }
 
-    if (magnetState >= 1000) {
-      bot.sendMessage(chat_id, "ALARM utløst", "");
-    }
-
     if (text == "/System_test") {
-      bot.sendMessage(chat_id, "System message test active", "");
       for (int i = 0; i <= 8; i++){
         digitalWrite(2, HIGH);
         delay(200);
         digitalWrite(2, LOW);
         delay(200);
       }
+      bot.sendMessage(chat_id, "System message test active", "");
     }
     
     if (text == "/siren_toggle") {
@@ -225,7 +300,17 @@ void handleNewMessages(int numNewMessages) {
     if (text == "/siren_burst") {
       sirenBURST();
       bot.sendMessage(chat_id, "Siren is bursting", "");
-      
+    }
+    if (text == "/status") {
+      bot.sendMessage(chat_id, "SirenPin25 is currently " + String(digitalRead(sirenPin)), "");
+    }
+    if (text == "/awaken") {
+      SystemState = active;
+      bot.sendMessage(chat_id, "Sentry-mode Active", "");
+    }
+    if (text == "/sleep") {
+      SystemState = inactive;
+      bot.sendMessage(chat_id, "Cerberus is asleep", "");
     }
   }
 }
